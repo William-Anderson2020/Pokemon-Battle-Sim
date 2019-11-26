10943153c88eea5aa045ca505ec8cd7b3163cb93
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-26 10:37:29 -0500

    Check for valid entry hotfix

--- src/pkmnSelect.js
+++ src/pkmnSelect.js
@@ -14,36 +14,31 @@ export function pkmnSelect() {
     form.innerHTML = "";
     form.innerHTML = `<div class="pkmn_select"> <input class="pkmn_select_name "type="text" value="Name a Pokemon" onfocus="this.value = ''"> <input type="submit" class="pkmn_select_submit"> </div> <div class="selection_sprite_holder"> </div> <div class="selection_VS"> </div> <div class="c_sprite_holder"> </div> </div>`;
     
-    clickListener(".pkmn_select_submit", async function() {
+    clickListener(".pkmn_select_submit", async function partyBuild() {
       //Party Builder
       let selection = document.querySelector(".pkmn_select_name").value.toLowerCase();
-      let pkmn = await get.pkmn(selection);
-      /* let pkmn = async function userSelection(){
-        let selection = document.querySelector(".pkmn_select_name").value.toLowerCase();
-        data = await get.pkmn(selection);
-        console.log(data);
-        if(data = undefined){
-          userSelection();
-        }
-        else{
-          return pkmn;
-        }
-      } */
-      let cPkmn = await get.pkmn(Math.round(Math.random() * 807))
-      uParty.push(pkmn);
-      cParty.push(cPkmn);
+      let data = await get.pkmn(selection);
+      if(data == undefined){
+      return;
+      }
+      else{
+        let pkmn = await data;
+        let cPkmn = await get.pkmn(Math.round(Math.random() * 807))
+        uParty.push(pkmn);
+        cParty.push(cPkmn);
 
-      document.querySelector(".selection_sprite_holder").insertAdjacentHTML("beforeend",`<img src="${pkmn.sprites.front_default}" class="selection_sprite">`);
-        if(uParty.length == count){
-          document.querySelector('.pkmn_select').innerHTML = '<input type="submit" name="pkmn_confirm" id="pkmn_confirm" value="Proceed">'
-          document.querySelector('.selection_VS').innerHTML= 'VS'
-          cParty.forEach((el) => {
-            document.querySelector('.c_sprite_holder').insertAdjacentHTML("beforeend", `<img src="${el.sprites.front_default}" class="selection_sprite">`);
-          });
-          clickListener('#pkmn_confirm', function(){
-            partyInit(uParty, cParty);
-          });
-        }
+        document.querySelector(".selection_sprite_holder").insertAdjacentHTML("beforeend",`<img src="${pkmn.sprites.front_default}" class="selection_sprite">`);
+          if(uParty.length == count){
+            document.querySelector('.pkmn_select').innerHTML = '<input type="submit" name="pkmn_confirm" id="pkmn_confirm" value="Proceed">'
+            document.querySelector('.selection_VS').innerHTML= 'VS'
+            cParty.forEach((el) => {
+              document.querySelector('.c_sprite_holder').insertAdjacentHTML("beforeend", `<img src="${el.sprites.front_default}" class="selection_sprite">`);
+            });
+            clickListener('#pkmn_confirm', function(){
+              partyInit(uParty, cParty);
+            });
+          }
+      }
     });
   });
-}
+};
--- src/turnInit.js
+++ src/turnInit.js
@@ -1,3 +1,4 @@
 export function turnInit(uParty, cParty){
-    
+    let window = document.querySelector('.window');
+    let textbox = document.querySelector('.textbox')
 }
